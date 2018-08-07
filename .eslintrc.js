6706354cadab885c8278a2473963988a25a144d6
Mikel Loidi <mikel.loidi@outlook.com>
2018-08-07 19:51:24 +0200

    First commit

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "rules": {
+        "indent": [ 2, 2 ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-console": 0
+    },
+    "parserOptions": {
+        "sourceType": "module"
+    }
+};
